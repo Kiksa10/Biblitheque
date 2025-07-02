c8194cc09023b888ec97da876e96c66c5a3d99b9
Kiksa10 <fitiafrancky10@gmail.com>
2025-07-02 21:53:50 +0300

    creation page inscription et test pour le traitement login

---
+++ src/main/java/controller/ConnexionTraitement.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class ConnexionTraitement {
+    
+
+    @PostMapping("/login")
+    public String traitement_Login(){
+
+        return "dashboard";
+    }
+}
--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -8,8 +8,21 @@ public class LoginController {
     
 
     @GetMapping("/")
-    public String redirectToList() {
+    public String redirectToLogin() {
         System.out.println("Redirection vers /login");
         return "index";
     }
+
+    @GetMapping("/login")
+    public String redirectToLog() {
+        System.out.println("Redirection vers /login");
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String redirectToRegistre() {
+        System.out.println("Redirection vers /Inscription");
+        return "inscription";
+    }
+    
 }
